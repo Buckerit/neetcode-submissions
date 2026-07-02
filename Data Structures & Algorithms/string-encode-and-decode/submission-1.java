709b6511664ba879d365af397ef665dd3a47eb9c
Buckerit <rothe.aarav@gmail.com>
2026-07-01 20:10:40 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        if (strs.isEmpty()) return "";
+        StringBuilder res = new StringBuilder();
+        List<Integer> sizes = new ArrayList<>();
+
+        for (String string : strs) {
+            sizes.add(string.length());
+        }
+        for (int size : sizes) {
+            res.append(size).append(',');
+        }
+        res.append("#");
+        for (String string : strs) {
+            res.append(string);
+        }
+
+        return res.toString();
+        
+
+    }
+
+    public List<String> decode(String str) {
+        if (str.length() == 0) {
+            return new ArrayList<>();
+        }
+        List<Integer> sizes = new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while (str.charAt(i) != '#') {
+            StringBuilder cur = new StringBuilder();
+            while (str.charAt(i) != ',') {
+                cur.append(str.charAt(i));
+                i++;
+            }
+            sizes.add(Integer.parseInt(cur.toString()));
+            i++;
+        }
+        i++;
+        for (int size : sizes) {
+            res.add(str.substring(i, i + size));
+            i += size;
+        }
+        return res;
+    }
+}
